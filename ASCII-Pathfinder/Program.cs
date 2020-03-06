e8ff60b10eb5f222ae069d2292579a8107d726b0
Bruno Pavec <bruno.pavec@gmail.com>
2020-03-06 11:10:39 +0100

    Initial project

---
+++ ASCII-Pathfinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASCII_Pathfinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
